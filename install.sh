5917940badea9b21019bffa505fcdf17cd1a3c5c
Benedikt Meurer <bmeurer@google.com>
2020-05-28 08:10:28 +0200

    [gnome-terminal] Use Dracula color theme.

--- install.sh
+++ install.sh
@@ -66,20 +66,11 @@ if [[ -x /usr/bin/gsettings ]]; then
 		value=$(echo $line | cut -f2- -d' ')
 		/usr/bin/gsettings set ${gschema}:${gpath} $key "$value"
 	done <<EOF
-foreground-color 'rgb(131,148,150)'
-highlight-foreground-color '#ffffff'
-palette ['rgb(7,54,66)', 'rgb(220,50,47)', 'rgb(133,153,0)', 'rgb(181,137,0)', 'rgb(38,139,210)', 'rgb(211,54,130)', 'rgb(42,161,152)', 'rgb(238,232,213)', 'rgb(0,43,54)', 'rgb(203,75,22)', 'rgb(88,110,117)', 'rgb(101,123,131)', 'rgb(131,148,150)', 'rgb(108,113,196)', 'rgb(147,161,161)', 'rgb(253,246,227)']
-cursor-background-color '#000000'
 scrollbar-policy 'always'
 use-system-font false
-bold-color '#000000'
-cursor-foreground-color '#ffffff'
 visible-name 'Benedikt'
 scrollback-unlimited true
 font 'JetBrains Mono 12'
-highlight-colors-set false
-use-theme-colors false
-background-color 'rgb(0,43,54)'
-highlight-background-color '#000000'
 EOF
+	(cd gnome-terminal && ./install.sh -s Dracula -p Benedikt --skip-dircolors)
 fi
